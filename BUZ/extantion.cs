91ad027af24d7c79ad02e405f60c6ff233a08647
Yarikbuz <Yarikbuz@mail.ru>
2024-03-26 20:17:16 +0300

    класс с расширением print

---
+++ BUZ/extantion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUZ
+{
+    static class Extansion
+    {
+        public static T[] Print<T>(this T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write(item+"\t");
+
+
+            }
+            Console.WriteLine();
+            return array;
+        }
+        public static T Print<T>(this T item)
+        {
+            Console.WriteLine(item);
+            return item;
+        }
+    }
+}
